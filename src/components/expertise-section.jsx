bc458c00aa43ff7431f2844321f95ba07e973457
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-01-12 18:45:25 +0500

    lenob-project - expertise, animation on rings

--- src/components/expertise-section.jsx
+++ src/components/expertise-section.jsx
@@ -1,3 +1,5 @@
+import Marquee from 'react-fast-marquee';
+import { expertise } from '../lib/constants';
 import MaxWidthWrapper from './max-width-wrapper';
 import HeadingWithEllipse from './ui/heading-with-ellipse';
 
@@ -6,7 +8,24 @@ const ExpertiseSection = () => {
     <div className="w-full">
       <MaxWidthWrapper>
         <div className="pt-20 w-full">
-          <HeadingWithEllipse heading={"Grow Smarter, Faster, and Stronger with Our Proven Expertise"} width={"w-4/5"} />
+          <HeadingWithEllipse heading={"Grow Smarter, Faster, and Stronger with Our Proven Expertise"} />
+          <Marquee>
+            {
+              expertise.map((expertise)=>(
+                <div className='bg-white my-12 mx-3 p-3 w-[500px]'>
+                  <div className=' bg-red-600 '>
+                      <img src={expertise.banner} className='object-cover h-60' alt={`${expertise.title} banner`} />
+                  </div>
+                  <div className='my-8'>
+                    <p>{expertise.title}</p>
+                    <p>{expertise.tag}</p>
+                    <p>{expertise.description}</p>
+                  </div>
+
+                </div>
+              ))
+            }
+          </Marquee>
         </div>
       </MaxWidthWrapper>
     </div>
---
+++ src/lib/constants.js
@@ -0,0 +1,35 @@
+import CROBanner from "../assets/img/cro-banner.png";
+import FacebookAdsBanner from "../assets/img/facebook-ads-banner.png";
+import GoogleAdsBanner from "../assets/img/google-ads-banner.png";
+import SEOBanner from "../assets/img/seo-banner.png";
+
+export const expertise = [
+  {
+    id: 1,
+    title: "Conversion Rate Optimization (CRO)",
+    tag: "Turn clicks into customers.",
+    description: "With seamless user experiences and data-backed optimizations, we help you maximize the value of every visitor.",
+    banner: CROBanner,
+  },
+  {
+    id: 2,
+    title: "Google Ads Management",
+    tag: "Boost visibility, capture leads, and maximize ROI.",
+    description: "With seamless user experiences and data-backed optimizations, Google Ads Management help you maximize the value of every visitor.",
+    banner: GoogleAdsBanner,
+  },
+  {
+    id: 3,
+    title: "Facebook & Instagram Ads",
+    tag: "Connect, engage, and convert effortlessly",
+    description: "Transform your social media presence with visually captivating ads and audience-specific targeting that turns followers into loyal customers.",
+    banner: FacebookAdsBanner,
+  },
+  {
+    id: 4,
+    title: "SEO Services",
+    tag: "Get found, stay relevant, and grow organically.",
+    description: "Our comprehensive SEO strategies drive consistent, high-quality traffic while establishing your authority in the market.",
+    banner: SEOBanner,
+  },
+]
