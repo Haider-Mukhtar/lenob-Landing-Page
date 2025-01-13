5253694189cb69cf1fcb6c5b8d9b52c09d73a428
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-01-13 12:53:25 +0500

    lenob-project - steps section responsive

--- src/components/steps-section.jsx
+++ src/components/steps-section.jsx
@@ -9,14 +9,14 @@ const StepsSection = () => {
       <MaxWidthWrapper>
         <div className="pt-20 w-full">
           <HeadingWithEllipse heading={"From Traffic to Conversions"} />
-          <p className="text-4xl font-medium mt-10 text-center">The Complete Solution for Your Business Growth</p>
-          <div className="grid grid-cols-4 gap-8">
+          <p className="text-xl sm:text-2xl lg:text-4xl font-medium mt-10 text-center">The Complete Solution for Your Business Growth</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-8 my-10">
             {
               stepsData.map((stepsData)=>(
-                <div key={stepsData} className="text-black flex flex-col items-center gap-8 my-10">
-                  <p className="font-bold text-5xl">{stepsData.step}</p>
-                  <p className="font-bold text-3xl text-center px-8">{stepsData.title}</p>
-                  <p className="text-xl text-center">{stepsData.description}</p>
+                <div key={stepsData} className="text-black flex flex-col items-center gap-8">
+                  <p className="font-bold text-4xl sm:text-5xl md:text-5xl xl:text-6xl">{stepsData.step}</p>
+                  <p className="font-bold text-2xl sm:text-3xl lg:text-3xl text-center px-12 xl:px-8">{stepsData.title}</p>
+                  <p className="text-lg sm:text-xl text-center">{stepsData.description}</p>
                 </div>
               ))
             }
