1e39f67af8517db8401c8bfac434fe358b0faee2
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-01-13 08:55:41 +0500

    lenob-project - responsive expertise and growth

--- src/components/expertise-section.jsx
+++ src/components/expertise-section.jsx
@@ -10,17 +10,17 @@ const ExpertiseSection = () => {
       <MaxWidthWrapper>
         <div className="pt-20 w-full">
           <HeadingWithEllipse heading={"Grow Smarter, Faster, and Stronger with Our Proven Expertise"} />
-          <Marquee pauseOnHover pauseOnClick >
+          <Marquee pauseOnHover>
             {
               expertise.map((expertise)=>(
-                <div key={expertise.id} className='relative my-12 mx-4 p-4 w-[500px] rounded-xl duration-500 hover:scale-105 drop-shadow-lg bg-white/50 '>
-                  <div className=' bg-black overflow-hidden rounded-lg h-60'>
-                      <img src={expertise.banner} className='object-cover w-full h-full' alt={`${expertise.title} banner`} />
-                  </div>
-                  <div className='my-6 text-black'>
-                    <p className='font-bold text-2xl'>{expertise.title}</p>
-                    <p className='text-xs mt-1 text-gray-800'>{expertise.tag}</p>
-                    <p className='text-lg mt-4 text-gray-900'>{expertise.description}</p>
+                <div key={expertise.id} className='relative my-12 mx-2 md:mx-4 p-4 w-80 sm:w-96 md:w-[500px] rounded-xl duration-500 hover:scale-105 drop-shadow-lg bg-white/50 '>
+                  <div className='flex flex-col justify-between items-center'>
+                    <img src={expertise.banner} className='object-cover w-full h-60 bg-black rounded-lg' alt={`${expertise.title} banner`} />
+                    <div className='my-4 md:my-6 text-black'>
+                      <p className='font-bold text-lg sm:text-xl md:text-2xl'>{expertise.title}</p>
+                      <p className='text-xs mt-1 text-gray-800'>{expertise.tag}</p>
+                      <p className='text-base md:text-lg mt-4 text-gray-900'>{expertise.description}</p>
+                    </div>
                   </div>
                   <button className='bg-black p-1.5 rounded-full absolute top-8 right-8 drop-shadow-md hover:scale-105 duration-200'>
                     <FaRegHeart className='size-5 fill-white aspect-square' />
@@ -29,11 +29,11 @@ const ExpertiseSection = () => {
               ))
             }
           </Marquee>
-          <div className="flex items-center justify-center gap-4 md:gap-6 lg:gap-10">
+          <div className="flex items-center justify-center">
             <button className="rounded-xl border-[2.5px] border-black bg-black px-8 py-3.5 text-xl font-semibold text-white transition-colors duration-200 hover:bg-transparent hover:text-black">
               Get a Free Digital Audit
             </button>
-        </div>
+          </div>
         </div>
       </MaxWidthWrapper>
     </div>
--- src/components/growth-section.jsx
+++ src/components/growth-section.jsx
@@ -9,19 +9,19 @@ const GrowthSection = () => {
       <MaxWidthWrapper>
         <div className="pt-20 w-full">
           <HeadingWithEllipse heading={"Why Businesses out there Trust Lenob for Growth"} />
-          <div className="grid grid-cols-3 my-12 px-24">
+          <div className="grid grid-cols-1 md:grid-cols-3 py-10 lg:px-10 xl:px-24">
             <div className="col-span-2">
               {
                 growthContent.map((growthContent)=>(
-                  <div key={growthContent.id} className="space-y-6 my-6">
-                    <p className="font-bold text-3xl">{growthContent.title}</p>
-                    <p className="text-2xl">{growthContent.description}</p>
+                  <div key={growthContent.id} className="space-y-4 my-4 md:space-y-6 md:my-6">
+                    <p className="font-bold text-2xl md:text-3xl">{growthContent.title}</p>
+                    <p className="text-lg md:text-2xl">{growthContent.description}</p>
                   </div>
                 ))
               }
             </div>
-            <div className="flex justify-center items-center -rotate-12">
-              <img src={growthRateImage} alt="growthRateImage" className="aspect-square object-fill" />
+            <div className="flex justify-center items-center">
+              <img src={growthRateImage} alt="growthRateImage" className="aspect-square size-72 sm:size-80 md:size-auto object-fit -rotate-12" />
             </div>
           </div>
         </div>
